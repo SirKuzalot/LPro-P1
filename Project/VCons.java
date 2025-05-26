3f3e60a23260ef0b8d28808d92ff1d46a8d0bd8c
André Peres de Noronha <andre.peres.noronha@tecnico.ulisboa.pt>
2025-05-26 17:58:14 +0100

    fix: remove old line

--- Project/VCons.java
+++ Project/VCons.java
@@ -1,13 +1,11 @@
 public class VCons implements IValue {
     private final IValue head;
     private final IValue tail;
-    private final boolean isLazy;
 
     // Strict cons
     public VCons(IValue head, IValue tail) {
         this.head = head;
         this.tail = tail;
-        this.isLazy = false;
     }
 
     public IValue getHead() { return head; }
